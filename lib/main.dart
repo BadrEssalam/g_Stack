6598aa7a3f57c32d32019f584fe4aa5c79315080
BadrEssalam <badressalambdrilling@gmail.com>
2024-12-28 08:51:51 +0100

    begin the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(const MyWidget());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(),
-      ),
-    );
+    return const Placeholder();
   }
 }
